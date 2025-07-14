34bcde641a63d3b1f579a66089a8f8d58b1dabc0
RedjanVisitacion <visitacionredjan@gmail.com>
2025-07-14 17:53:22 +0800

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Directionality(
       textDirection: TextDirection.ltr,
-      child: Center(child: Text('My first practice app')),
+      child: Center(child: Text('My first practice appk')),
     );
   }
 }
